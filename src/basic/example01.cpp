9fca0e3a96282b39cedd453fe174944ec0817089
Vinci Xu <xsiyuan@gmail.com>
2023-07-21 17:03:56 +0800

    cin & cout

--- src/basic/example01.cpp
+++ src/basic/example01.cpp
@@ -1,11 +1,13 @@
+#include <cstddef>
 #include <iostream>
 #include <string>
 
+// 基本输入输出流入门
 int main(int argc, char *argv[]) {
   std::string num1, num2;
-  std::cout << "输入 number1: " << std::endl;
+  std::cout << "输入 number1: ";
   std::cin >> num1;
-  std::cout << "enter number 2: " << std::endl;
+  std::cout << "enter number 2: ";
   std::cin >> num2;
 
   int number1 = stoi(num1);
@@ -13,5 +15,14 @@ int main(int argc, char *argv[]) {
 
   std::cout << number1 << "+" << number2 << "=" << number1 + number2
             << std::endl;
+
+  std::printf("%d + %d = %d\n", number1, number2, number1 + number2);
+
+  if (argc != 1) {
+    std::cout << "you input " << argc << " arguments" << std::endl;
+    for (size_t i = 0; i < argc; i++) {
+      std::cout << "第" << i << "个arg是：" << argv[i] << std::endl;
+    }
+  }
   return 0;
 }
